28a1eb62060c4d2fb6c103676673a526831430aa
phongismyname <72543323+phongismyname@users.noreply.github.com>
2020-11-26 17:03:52 +0700

    Create chuvi.c

---
+++ Bài tập 1/chuvi.c
@@ -0,0 +1,13 @@
+#include <stdion.h>
+
+define PT 3.14
+int main(void)
+{
+printf("PT:%.2F',PI
+// goi ham  print
+print("Hello world!\n");
+/* gọi hàm getchar
+và kết thúc */
+getchar();
+return 0 ;
+{
