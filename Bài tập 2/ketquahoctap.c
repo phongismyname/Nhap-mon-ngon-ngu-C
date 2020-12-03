3cf16b61c6644da4c2e250d4b548a6c6bbf755d3
phongismyname <72543323+phongismyname@users.noreply.github.com>
2020-12-03 02:25:25 -0700

    Create ketquahoctap.c

---
+++ Bài tập 2/ketquahoctap.c
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+{
+   cout<<"tinh diem trung binh"<<endl;
+   cout<<"moi ban nhao diem toan, van,"<<endl;
+   float toan,van;//khai bao bien toan ly hoa
+   cin>>toan;//nhap vao diem toan
+   cin>>van;
+   
+   float dtb =(toan+van)/2; 
+   count <<"diem trung binh la: "<<dtb<<endl;
+   
+   return 0;
+}
