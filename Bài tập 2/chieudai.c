0e874991e592a2a18baca9a2c5ab94f1147c1cf5
phongismyname <72543323+phongismyname@users.noreply.github.com>
2020-12-03 01:46:03 -0700

    Create chieudai.c

---
+++ Bài tập 2/chieudai.c
@@ -0,0 +1,14 @@
+#include <studio.h.
+
+mani(void){
+//khai bao bien c,m,,l
+double c,m,l;
+//hien thi nhap l
+printf("nhap do dai can chuyen doi l = ");
+scanf("%cm",&l);
+c=l*100;
+m=1*1000;
+print("\n Do dai chuyen sang don vi xentimet la : %.f",i);
+print("\n Do dai chuyen sang don vi  milimet la : %.f",i);
+return 0;
+}
