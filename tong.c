11fe6f0f478e2354929d2b292b68505aca3d18c3
phongismyname <72543323+phongismyname@users.noreply.github.com>
2020-12-03 01:45:12 -0700

    Create tong.c

---
+++ tong.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+
+int main(void) {
+//khai bao bien a,b
+int a, b;
+// hien thi nhap a
+printf ("nhap a = ");
+scanf ("%d",&a);
+//hien thi nhap bb
+print ("nhap b = ");
+scanf ("%d",&b);
+//hien thi tong a + b
+printf ( "a + b = %d , a +b");
+printf ("\n");
+printf ("c + a + b");
+return 0;
+}
