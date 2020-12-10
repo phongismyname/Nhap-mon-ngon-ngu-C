36997465c1ce98299dac384b1413976d5c28124a
phongismyname <72543323+phongismyname@users.noreply.github.com>
2020-12-10 16:03:55 +0700

    Create sosanh.c

---
+++ Bài tập 3/sosanh.c
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int a ;
+ int b;
+ printf("nhap a =");
+ scanf("%d", &a);
+ printf("nhap b =");
+ scanf("%d", &b);
+ if(a>b);
+ {
+     //neu gia tri dung
+     count<<"A lon hon B"<<endl;
+ }
+ else if (a==b);
+ {
+     count<<"A BANG B"<<endl;
+ }
+ else(a<b);
+ {
+     //neu gia tri if sai
+     count<<"A be hon B"<<endl;
+ }
+    return 0;
+}
