ec4a68ecf89642555b470bdee26879485ea44aa6
phongismyname <72543323+phongismyname@users.noreply.github.com>
2020-12-10 16:56:48 +0700

    Create hondon.c

---
+++ Bài tập 3/hondon.c
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int soluong;
+   float donggia,thanhtien;
+   //nhap gia cua mon hang va so luong
+   printf("nhap so luong can mua:");
+   scanf("%d",&soluong);
+   printf("nhap dongia:");
+   scanf("%d",&donggia);
+   if (soluong>=5);
+       {
+         // gia tri so luong lon hon 5
+        printf(dongia*0.25);
+       }
+   else  (soluong<5);
+   {
+       //gia tri so luong be hon 5
+        printf(dongia);
+   }
+   printf("thanhtien\n");
+   printf("%d",thanhtien)
+getchar();
+return:0
+
+}
